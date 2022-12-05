b3eaccb2c7705b6e21197d07dd9e30261a0ba922
BLacey99 <81555350+BLacey99@users.noreply.github.com>
2022-12-05 14:52:39 -0600

    first commit

---
+++ library.js
@@ -0,0 +1,161 @@
+
+
+let myLibrary = [];
+let id = 0;
+
+function Book(name, author, pagecount, status) {
+  const bookObject = [];
+  
+  id++;
+  bookObject.id = id;
+  bookObject.name = name;
+  bookObject.author = author;
+  bookObject.pagecount = pagecount;
+  bookObject.status = status
+
+  return bookObject;
+}
+
+addBook=(bookObject)=> {
+  myLibrary.push(bookObject);
+};
+
+
+const container = document.getElementById("container");
+//Calls back to printInfo. Each item in myLibrary is a bookObject. Passed to printInfo parameter as an argument. Performs printInfo and returns.
+displayLibrary=()=> {
+  myLibrary.forEach(printInfo);
+};
+
+printInfo=(bookObject)=>{
+  console.log(Object.values(bookObject));
+  var readBtn = document.createElement('button');
+  readBtn.innerHTML=("Read?");
+  var removeBookBtn = document.createElement('button');
+  removeBookBtn.innerHTML=("REMOVE BOOK");
+  
+  removeBookBtn.onclick = removeBook=()=> {
+        for(i = 0; i<myLibrary.length; i++){
+          if (myLibrary[i].name ==bookObject.name){
+            myLibrary.splice(i, 1);
+          }
+        }
+    console.log(
+      `\nRemoved ${bookObject.name}\n---------------------------------------`
+    );
+  
+
+    container.removeChild(elemDiv);
+    console.log(myLibrary);
+    
+  };
+
+  readBtn.onclick = markRead=()=>{
+    for(i=0; i<myLibrary.length; i++){
+      if(myLibrary[i].name==bookObject.name){
+      
+    
+        
+        if(bookObject.status == "Unread"){
+          readBtn.style.cssText="width:50%; height:5%; margin-left:auto; margin-right:auto;margin-top:5%;margin-bottom:auto;background-color:green;color:white;";
+          bookObject.status="Read";
+          console.log(bookObject.status);
+      
+        }
+        else if(bookObject.status == "Read"){
+          readBtn.style.cssText="width:50%; height:5%; margin-left:auto; margin-right:auto;margin-top:5%;margin-bottom:auto;background-color:red;color:white;";
+          bookObject.status = "Unread";
+          console.log(bookObject.status);
+        }
+      }
+    }
+  }
+
+ // const bookDestructured = (Object.values(bookObject));
+  var elemDiv = document.createElement('div');
+  var topColor = document.createElement('div');
+  var botColor = document.createElement('div');
+  var bookTitle = document.createElement('h2');
+  bookTitle.innerHTML = bookObject.name;
+
+
+  var bookAuthor = document.createElement('h4');
+  bookAuthor.innerHTML =bookObject.author;
+
+  var bookPageCount = document.createElement('p');
+  bookPageCount.innerHTML = bookObject.pagecount;
+
+  var bookStatus = document.createElement('p');
+  bookStatus.innerHTML = bookObject.status;
+
+
+  elemDiv.append(topColor, bookTitle,bookAuthor, bookPageCount, bookStatus, readBtn, removeBookBtn, botColor);
+
+
+  
+  container.appendChild(elemDiv);
+  topColor.style.cssText="width:100%; height:15%; background-color:rgb(140, 47, 42);border-radius:12px 12px 0 0;";
+  botColor.style.cssText="width:100%; height:15%; background-color:rgb(140, 47, 42);border-radius:0 0 12px 12px;";
+  removeBookBtn.style.cssText='width:50%; height:5%; margin-left:auto; margin-right:auto;margin-top:3%;margin-bottom:2%;';
+
+  if(bookObject.status=="Read"){
+    readBtn.style.cssText='width:50%; height:5%; margin-left:auto; margin-right:auto;margin-top:5%;margin-bottom:auto;background-color:green;color:white;';
+  }else if(bookObject.status=="Unread"){
+    readBtn.style.cssText='width:50%; height:5%; margin-left:auto; margin-right:auto;margin-top:5%;margin-bottom:auto;background-color:red;color:white;';
+  }
+ 
+  elemDiv.style.cssText='border:2px solid black;background-color:#f6e4b0;width:250px; height:400px;text-align:center;display:flex;flex-direction:column; border-radius:15px;';
+  container.style.cssText='gap:2px; border:3px black solid;height:100%;width:80%;padding:10px;display:flex;flex-direction:row;justify-content:center;margin-left:auto;margin-right:auto;flex-wrap:wrap;';
+  
+  elemDiv.addEventListener('mouseover',function(){
+    elemDiv.style.cssText='scale:105%;border:2px solid black;background-color:#f6e4b0;width:250px; height:400px;text-align:center;display:flex;flex-direction:column;border-radius:15px;';
+});
+elemDiv.addEventListener('mouseleave',function(){
+  elemDiv.style.cssText='scale:100%;border:2px solid black;background-color:#f6e4b0;width:250px; height:400px;text-align:center;display:flex;flex-direction:column;border-radius:15px;';
+})};
+
+
+const testBook = new Book(
+  "BookOne",
+  "Jane Doe",
+  "350",
+  "Unread"
+);
+
+
+const testTwo = new Book(
+  "BookTwo",
+  "None Auth",
+  "312",
+  "Read"
+);
+
+const testThree = new Book(
+  "BookThree",
+  "None Auth",
+  "312",
+  "Read"
+);
+
+const testFour = new Book(
+  "BookFour",
+  "None Auth",
+  "312",
+  "Unread"
+);
+
+const testFive = new Book(
+  "BookFive",
+  "None Auth",
+  "312",
+  "Unread"
+);
+
+addBook(testBook);
+addBook(testTwo);
+addBook(testThree);
+addBook(testFour);
+addBook(testFive);
+displayLibrary();
+
+
